b9aa68bb820507e7d965104d10002e772c98adda
Wolpert Tobias GCR EOQMM4 <tobias.wolpert@gmail.com>
2022-03-28 21:58:47 -0400

    Fetching data from API and rendering recipe

--- src/components/header.components.jsx
+++ src/components/header.components.jsx
@@ -1,36 +1,13 @@
-import React from "react";
-
-(async () => {
-  try {
-    const result = await fetch(
-      "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
-    );
-    const data = await result.json();
-    console.log(result);
-    console.log("Data", data);
-
-    let { recipe } = data.data;
-    recipe = {
-      cookingTime: recipe.cooking_time,
-      id: recipe.id,
-      image: recipe.image_url,
-      ingredients: recipe.ingredients,
-      publisher: recipe.publisher,
-      servings: recipe.servings,
-      sourceUrl: recipe.source_url,
-      title: recipe.title,
-    };
-    console.log(recipe);
-
-    if (!result.ok) {
-      throw new Error(`${data.error}. Error: ${result.status}`);
-    }
-  } catch (err) {
-    alert(err);
-  }
-})();
+import React, { useState } from "react";
 
 const Header = () => {
+  const [search, setSearch] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(search);
+  };
+
   return (
     <header className="header">
       <div className="logo">
@@ -40,11 +17,15 @@ const Header = () => {
         <div className="logo__text">Food App</div>
       </div>
 
-      <form className="search">
+      <form className="search" onSubmit={handleSubmit}>
         <input
           className="search__input"
           type="text"
           placeholder="Search over 1.000.000 recipes..."
+          value={search}
+          onChange={(e) => {
+            setSearch(() => e.target.value);
+          }}
         ></input>
         <button className="search__button">
           <ion-icon name="search-outline"></ion-icon>
--- src/components/results.component.jsx
+++ src/components/results.component.jsx
@@ -1,13 +1,102 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Results = () => {
+  const [recipeData, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  // const [isError, setIsError] = useState(false);
+
+  console.log("1 Before useEffect", isLoading);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log("1.5 Fetch started", isLoading);
+      const result = await fetch(
+        "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
+      );
+      const data = await result.json();
+      let { recipe } = data.data;
+
+      recipe = {
+        cookingTime: recipe.cooking_time,
+        id: recipe.id,
+        image: recipe.image_url,
+        ingredients: recipe.ingredients,
+        publisher: recipe.publisher,
+        servings: recipe.servings,
+        sourceUrl: recipe.source_url,
+        title: recipe.title,
+      };
+      console.log("Async Test", recipe.ingredients[0]);
+      setData(recipe);
+      setIsLoading(false);
+    };
+    setIsLoading(true);
+    fetchData();
+    console.log("2 During useEffect", isLoading);
+  }, []);
+
+  // console.log("Hook 1", recipeData);
+  // console.log("Ingredients", recipeData.ingredients[0]);
+  console.log("3 Render", recipeData.ingredients, isLoading);
+
+  // const result;
+
+  // if (recipeData.ingredients !== undefined) {
+  //   console.log("4", recipeData.ingredients[0]);
+  //   recipeData.ingredients.forEach((item, idx) => console.log(idx, item));
+  // }
   return (
     <div className="container">
       <div className="container__results">Hallo</div>
 
-      <div className="container__detail">
-        {/* <img className="container__detail--img" src="{}" sr */}
-      </div>
+      {isLoading ? (
+        <div>Loading</div>
+      ) : (
+        <div className="container__detail">
+          <div
+            className="container__detail--img"
+            style={{ backgroundImage: `url(${recipeData.image})` }}
+          >
+            <div className="container__detail--name">{recipeData.title}</div>
+          </div>
+
+          <div className="container__detail--individual">
+            <div className="container__detail--pair">
+              <ion-icon name="time-outline"></ion-icon>
+              <div>
+                <strong>{recipeData.cookingTime}</strong> Minutes
+              </div>
+            </div>
+
+            <div className="container__detail--pair">
+              <ion-icon name="people-outline"></ion-icon>
+              <div>
+                <strong>{recipeData.servings}</strong> Servings
+              </div>
+              <ion-icon name="remove-circle-outline"></ion-icon>
+              <ion-icon name="add-circle-outline"></ion-icon>
+            </div>
+
+            <div className="container__detail--pair">
+              <div className="container__detail--circle">
+                <ion-icon name="person-outline"></ion-icon>
+              </div>
+              <div className="container__detail--circle">
+                <ion-icon name="bookmark-outline"></ion-icon>
+              </div>
+            </div>
+          </div>
+
+          <div className="container__detail--ingredients">
+            <h1>Recipe Ingredients</h1>
+            <div className="container__detail--list">
+              <ul className="container__detail--ingredient">
+                {/* <li>{recipeData.ingredients[0]}</li> */}
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
