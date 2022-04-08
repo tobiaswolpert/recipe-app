342db86e6552e5f36f658b22e1ce8399224a39be
Wolpert Tobias GCR EOQMM4 <tobias.wolpert@gmail.com>
2022-04-08 15:03:44 -0400

    Added bookmark list with scrolling

--- src/components/header.components.jsx
+++ src/components/header.components.jsx
@@ -1,8 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getJSON } from "../utils/helpers";
+import { API_URL } from "../utils/config";
 
 const Header = (props) => {
   const [search, setSearch] = useState("");
-  const { setQuery } = props;
+  const [bookmarkList, setBookmarkList] = useState([]);
+  const [show, setShow] = useState(false);
+
+  const { setQuery, bookmarks } = props;
+
+  useEffect(() => {
+    const fetchBookmarks = async (bookmarks) => {
+      try {
+        const res = await Promise.all(
+          bookmarks.map((item) => getJSON(`${API_URL}/${item}`))
+        );
+        const recipe = res.map((item) => item.data.recipe);
+        console.log(recipe);
+        setBookmarkList(recipe);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchBookmarks(bookmarks);
+  }, [bookmarks]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -11,6 +32,14 @@ const Header = (props) => {
 
   const handleOnChange = (e) => {
     setSearch(e.target.value);
+  };
+
+  const handleClick = () => {
+    if (bookmarkList.length) {
+      setShow(!show);
+    } else {
+      return null;
+    }
   };
 
   return (
@@ -42,10 +71,26 @@ const Header = (props) => {
           <div className="control__text">Add recipe</div>
         </div>
 
-        <div className="control__bookmark">
-          <ion-icon name="bookmark-outline"></ion-icon>
+        <div className="control__bookmark" onClick={handleClick}>
+          <ion-icon name={show ? "bookmark" : "bookmark-outline"}></ion-icon>
           <div className="control__text">Bookmarks</div>
         </div>
+      </div>
+
+      <div className={show ? "bookmark_list" : "bookmark_list invisible"}>
+        {bookmarkList.map((item) => (
+          <li className="bookmark_list--item">
+            <img
+              className="bookmark_list--img"
+              src={item.image_url}
+              alt={item.title}
+            />
+            <div className="bookmark_list--details">
+              <div className="bookmark_list--title">{item.title}</div>
+              <div className="bookmark_list--publisher">{item.publisher}</div>
+            </div>
+          </li>
+        ))}
       </div>
     </header>
   );
